b608f8fe68744137039c7e73dfe4be9c0ffc7798
dibon001 <dibon001@ucr.edu>
2018-12-10 16:33:40 -0800

    Functionality of initial buttons work of displaying 10 GIFS with rating and then empties when another button is created

---
+++ js/logic.js
@@ -0,0 +1,57 @@
+console.log("No button clicked yet");
+
+$("button").on("click", function() {
+    $("#giphy-div").empty();
+
+    console.log("Button clicked");
+    // Grabbing and storing the data-animal property value from the button
+    var celeb = $(this).attr("data-person");
+
+    // Constructing a queryURL using the animal name
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
+      celeb + "&api_key=dc6zaTOxFJmzC&limit=10";
+
+    // Performing an AJAX request with the queryURL
+    $.ajax({
+      url: queryURL,
+      method: "GET"
+    })
+      // After data comes back from the request
+      .then(function(response) {
+        console.log(queryURL);
+
+        console.log(response);
+        // storing the data from the AJAX request in the results variable
+        var results = response.data;
+
+        // Looping through each result item
+        for (var i = 0; i < results.length; i++) {
+
+          // Creating and storing a div tag
+          var celebDiv = $("<div>");
+
+          // Creating a paragraph tag with the result item's rating
+          var p = $("<p>").text("Rating: " + results[i].rating);
+          //var p = $("<button>").text("Hello");
+
+
+          // Creating and storing an image tag
+          var celebImage = $("<img>");
+          // Setting the src attribute of the image to a property pulled off the result item
+          celebImage.attr("src", results[i].images.fixed_height.url);
+
+          // Appending the paragraph and image tag to the animalDiv
+          celebDiv.append(p);
+          celebDiv.append(celebImage);
+
+          // Prependng the animalDiv to the HTML page in the "#gifs-appear-here" div
+          $("#giphy-div").prepend(celebDiv);
+        }
+      });
+  });
+
+  $("#add-button").on("click", function(event) {
+      console.log("Submit has been pressed");
+        var newButton = $("<button>").text(add-more.val().trim());
+
+  });
